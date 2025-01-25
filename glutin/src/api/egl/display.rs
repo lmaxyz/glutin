d4904d2e64b84bd36322211f81ff625ec849a876
Maxim Leyman <maximleyman@yandex.ru>
2025-01-25 22:22:21 +0300

    added fix for devices with OS Aurora, that have android video driver but have wayland raw display handle

--- glutin/src/api/egl/display.rs
+++ glutin/src/api/egl/display.rs
@@ -265,6 +265,11 @@ impl Display {
             {
                 (egl::PLATFORM_WAYLAND_KHR, handle.display.as_ptr())
             },
+            RawDisplayHandle::Wayland(handle)
+                if extensions.contains("EGL_KHR_platform_android") =>
+            {
+                (egl::PLATFORM_ANDROID_KHR, handle.display.as_ptr())
+            },
             RawDisplayHandle::Xlib(handle) if extensions.contains("EGL_KHR_platform_x11") => {
                 attrs.push(egl::PLATFORM_X11_SCREEN_KHR as EGLAttrib);
                 attrs.push(handle.screen as EGLAttrib);
